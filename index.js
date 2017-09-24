2404e33e0c0bec7eb1e05667553355dd54c58d98
Wilson Novaes <wilsonnovaes00@hotmail.com>
2017-09-24 15:57:43 -0300

    Vamos la

--- index.js
+++ index.js
@@ -2,9 +2,6 @@ var http = require('http')
     ,app = require('./config/express')
     db = require('./config/database');
 
-
-    app.use(express.static(path.resolve(__dirname)));
-
 http.createServer(app).listen(3000, function() {
     console.log('Servidor escutando na porta: ' + this.address().port);
 });
