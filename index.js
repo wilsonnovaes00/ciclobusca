65e3f01e0ad9c9cf93d8487d31a9cdafc248be90
Wilson Novaes <wilsonnovaes00@hotmail.com>
2017-09-24 16:23:00 -0300

    teste

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ var http = require('http')
     ,app = require('./config/express')
     db = require('./config/database');
 
-    http.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
-        console.log('running...');
+http.createServer(app).listen(3000, function() {
+    console.log('Servidor escutando na porta: ' );
+
 });
 
