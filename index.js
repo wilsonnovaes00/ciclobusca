38297735750885d40552058d22f03d77d98ee756
Wilson Novaes <wilsonnovaes00@hotmail.com>
2017-09-24 16:21:48 -0300

    Tes

--- index.js
+++ index.js
@@ -2,9 +2,6 @@ var http = require('http')
     ,app = require('./config/express')
     db = require('./config/database');
 
-    app.use(express.static(path.resolve(__dirname + '/public')));
-
-
     http.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
         console.log('running...');
 });
