29a2a02f723fdfc8be3ebc1075d1abd1815c811f
Wilson Novaes <wilsonnovaes00@hotmail.com>
2017-09-24 16:17:19 -0300

    Tes

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ var http = require('http')
     ,app = require('./config/express')
     db = require('./config/database');
 
-    app.use(express.static(path.resolve(__dirname + '/public')));
-    
+    app.use(express.static(path.resolve(__dirname + '/public/index.html')));
+
 http.createServer(app).listen(3000, function() {
     console.log('Servidor escutando na porta: ' + this.address().port);
 });
